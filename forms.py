63def8f37b52a5ca388b989c9ea36a28c6e26d48
Kevin-Lai <kevinlai249@gmail.com>
2019-12-04 17:30:35 -0800

    Updated forms to handle autocomplete data

--- forms.py
+++ forms.py
@@ -5,36 +5,36 @@ Author: Kevin Lai
 '''
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, TextAreaField
-from wtforms.validators import InputRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email
 
 class ContactForm(FlaskForm):
 	first_name = StringField('first_name',
-							  validators=[InputRequired(), Length(min = 1, max = 50)])
+							  validators=[DataRequired(), Length(min = 1, max = 100)])
 	last_name = StringField('last_name',
-							  validators=[InputRequired(), Length(min = 1, max = 50)])
-	account_id = IntegerField('account_id', validators=[InputRequired()])
-	phone_number = IntegerField('phone_number', validators=[InputRequired()])
+							  validators=[DataRequired(), Length(min = 1, max = 100)])
+	account_id = IntegerField('account_id', validators=[DataRequired()])
+	phone_number = IntegerField('phone_number', validators=[DataRequired()])
 	email_address = StringField('email_address',
-							  validators=[InputRequired(), Email(message='INVALID_EMAIL_ADDRESS'), Length(min = 1, max = 256)])
+							  validators=[DataRequired(), Email(message='INVALID_EMAIL_ADDRESS'), Length(min = 1, max = 256)])
 	street_address = StringField('street_address', validators=[Length(max = 100)])
-	city = StringField('city', validators=[Length(max = 60)])
-	state = StringField('state',validators=[Length(max = 50)])
+	city = StringField('city', validators=[Length(max = 100)])
+	state = StringField('state',validators=[Length(max = 100)])
 	postal_code = IntegerField('postal_code')
-	country = StringField('country', validators=[Length(max = 55)])
+	country = StringField('country', validators=[Length(max = 100)])
 	notes = TextAreaField('notes')
 	save = SubmitField('Save')
 
 class AccountForm(FlaskForm):
 	name = StringField('first_name',
-						validators=[InputRequired(), Length(min = 1, max = 50)])
+						validators=[DataRequired(), Length(min = 1, max = 100)])
 	phone_number = IntegerField('phone_number',
-							  validators=[InputRequired()])
+							  validators=[DataRequired()])
 	email_address = StringField('email_address',
-							  validators=[InputRequired(), Email(message='INVALID_EMAIL_ADDRESS'), Length(min = 1, max = 256)])
-	street_address = StringField('street_address', validators=[InputRequired(), Length(max = 100)])
-	city = StringField('city', validators=[InputRequired(), Length(max = 60)])
-	state = StringField('state',validators=[InputRequired(), Length(max = 50)])
-	postal_code = IntegerField('postal_code', validators=[InputRequired()])
-	country = StringField('country', validators=[InputRequired(), Length(max = 55)])
+							  validators=[DataRequired(), Email(message='INVALID_EMAIL_ADDRESS'), Length(min = 1, max = 256)])
+	street_address = StringField('street_address', validators=[DataRequired(), Length(max = 100)])
+	city = StringField('city', validators=[DataRequired(), Length(max = 100)])
+	state = StringField('state',validators=[DataRequired(), Length(max = 100)])
+	postal_code = IntegerField('postal_code', validators=[DataRequired()])
+	country = StringField('country', validators=[DataRequired(), Length(max = 100)])
 	notes = TextAreaField('notes')	
 	save = SubmitField('Save')
